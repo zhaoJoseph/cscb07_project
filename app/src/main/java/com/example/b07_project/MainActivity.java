98cea264e1ac6a27a394da4a4f22c6cc0be9de63
jianli588 <82671025+jianli588@users.noreply.github.com>
2021-11-22 12:12:46 -0500

    finished login page

--- app/src/main/java/com/example/b07_project/MainActivity.java
+++ app/src/main/java/com/example/b07_project/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.b07_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import encryption.MD5Encryption;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void loginAccount(View view){
+        TextView emailText = (TextView) findViewById(R.id.userEmailAddress);
+        TextView passwordText = (TextView) findViewById(R.id.userPassword);
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+
+        MD5Encryption encrypt = new MD5Encryption();
+        String encryptedPassword = encrypt.encrypt(password);
+
+        //TODO check if encryptedPassword matches with data in the firebase, if yes go into the next page
+
+        emailText.setText("");
+        passwordText.setText("");
+
+    }
+
+    public void signupAccount(View view){
+        setContentView(R.layout.sign_up);
     }
 }
